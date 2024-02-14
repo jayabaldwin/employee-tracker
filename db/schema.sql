fcf6aca04175681057b52c56f7d09a4a63ba0e50
Jaya <148409249+jayabaldwin@users.noreply.github.com>
2024-02-15 10:35:16 +1100

    readme import

---
+++ db/schema.sql
@@ -0,0 +1,20 @@
+-- Database
+DROP DATABASE IF EXISTS department_db;
+CREATE DATABASE department_db;
+
+USE department_db;
+
+-- Department
+CREATE TABLE department (
+
+);
+
+-- Role
+CREATE TABLE role (
+    
+);
+
+-- Employee
+CREATE TABLE employee (
+    
+);
