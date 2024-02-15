360ee326d8de7b8aff6c6e3a75f03c4e2831dc1f
Jaya <148409249+jayabaldwin@users.noreply.github.com>
2024-02-15 12:18:04 +1100

    added schema.sql data

--- db/schema.sql
+++ db/schema.sql
@@ -1,22 +1,37 @@
 -- Build tables and databases
 
 -- Database
-DROP DATABASE IF EXISTS department_db;
-CREATE DATABASE department_db;
+DROP DATABASE IF EXISTS management_db;
+CREATE DATABASE management_db;
 
-USE department_db;
+USE management_db;
 
 -- Department
 CREATE TABLE department (
-
+    id INT PRIMARY KEY,
+    name: VARCHAR(30)
 );
 
 -- Role
 CREATE TABLE role (
-    
+    id INT PRIMARY KEY,
+    -- holds role title
+    title VARCHAR(30),
+    -- holds role salary
+    salary DECIMAL,
+    -- hold reference to department role belongs to
+    department_id INT
 );
 
 -- Employee
 CREATE TABLE employee (
-    
+    id INT PRIMARY KEY,
+    -- holds employee first and last name
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    -- hold reference to employee role
+    role_id INT, 
+    -- hold reference to another employee that is the manager of current employee
+    -- null if no manager
+    manager_id INT 
 );
