41a1fbc10ff6077962c514a11f087852e9b6042f
Jaya <148409249+jayabaldwin@users.noreply.github.com>
2024-02-19 22:10:15 +1100

    finalise project

--- db/schema.sql
+++ db/schema.sql
@@ -12,7 +12,10 @@ CREATE TABLE role (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL,
-    department_id INT NOT NULL
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE CASCADE
 );
 
 CREATE TABLE employee (
@@ -20,5 +23,11 @@ CREATE TABLE employee (
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT NOT NULL, 
-    manager_id INT
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON DELETE CASCADE,
+    manager_id INT,
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+    ON DELETE CASCADE
 );
---
+++ otherindex.js
@@ -0,0 +1,25 @@
+
+const menu = require("./lib/menu.js");
+
+// Use functions from modules
+// // View
+// employeeModule.viewAllEmployees();
+// employeeModule.viewByManager();
+// departmentModule.viewAllDepartments();
+// departmentModule.viewByDepartment();
+// roleModule.viewAllRoles();
+
+// // Add
+// employeeModule.addEmployee();
+// departmentModule.addDepartment();
+// roleModule.addRole();
+
+// // Update
+// employeeModule.updateEmployeeManager();
+// roleModule.updateEmployeeRole();
+
+// // Delete
+// departmentModule.deleteDepartment();
+// roleModule.deleteRole();
+
+menu();
